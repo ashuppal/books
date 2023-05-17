ea4a45dc400c7d12b6c318e89f528af0ad1de9b7
Ash Uppal <ashuppal@gmail.com>
2023-05-17 10:22:13 -0700

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,40 +1,83 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from 'axios';
 
 function App() {
 
 const [books, SetBooks] = useState([]);
 
-function createBook(title) {
- const updatedBooks = [
-  ...books,
-  {
-    id: Math.round(Math.random() * 9999),
-    title
-  }
- ]
- SetBooks(updatedBooks);
+const fetchBooks = async() => {
+  const response = await axios.get('http://localhost:3001/books');
+  SetBooks(response.data);
 }
 
-function deleteBookById(id) {
+useEffect(() => {
+  fetchBooks();
+}, []);
+
+
+const createBook= async(title)=> {
+  const response = await axios.post('http://localhost:3001/books', { title })
+
+  const newBook = response.data;
+  const updatedBooks = [
+    ...books,
+    newBook
+  ];
+  SetBooks(updatedBooks);
+
+//  const updatedBooks = [
+//   ...books,
+//   {
+//     id: Math.round(Math.random() * 9999),
+//     title
+//   },
+//  ];
+//  SetBooks(updatedBooks);
+}
+
+
+
+const deleteBookById = async(id) => {
+  await axios.delete(`http://localhost:3001/books/${id}`)
   const updatedBooks = books.filter((book) => {
     return book.id !== id;
   })
   SetBooks(updatedBooks);
+  
+  // const updatedBooks = books.filter((book) => {
+  //   return book.id !== id;
+  // })
+  // SetBooks(updatedBooks);
 }
-function editBookById(id, newTitle) {
+
+const editBookById = async(id, newTitle)=> {
+  //we put title:newTitle because we want to update the title of the book with the newTitle
+  const response = await axios.put(`http://localhost:3001/books/${id}`, { title: newTitle })
+ 
+
   const updatedBooks = books.map((book) => {
     if (book.id === id) {
-      return {
-        ...book,
-        title: newTitle
-      }
+  return {...book, ...response.data}
     } else {
       return book;
-    }
+    };
   })
   SetBooks(updatedBooks);
+
+ 
+  // const updatedBooks = books.map((book) => {
+  //   if (book.id === id) {
+  //     return {
+  //       ...book,
+  //       title: newTitle
+  //     }
+  //   } else {
+  //     return book;
+  //   }
+  // })
+  // SetBooks(updatedBooks);
 }
 
   return (
